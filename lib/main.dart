caf384f7efff72129c71b1f7ed30981897a1890a
Aurelien T. <aurelien.traini@gmail.com>
2024-10-29 21:31:29 +0100

    raveparty

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:chevaldetroie/view/concour.dart';
 import 'package:chevaldetroie/view/horseLesson.dart';
+import 'package:chevaldetroie/view/raveParty.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Horselesson(),
+      home: Raveparty(),
     );
   }
 }
