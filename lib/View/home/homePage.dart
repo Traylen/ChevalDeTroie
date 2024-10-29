b92a2e434a6c7ceb2fadbd1b99c82677ef4d245e
theo <theo.saint-amand@orange.fr>
2024-10-29 14:45:22 +0100

    fix:ignore-vscode

--- lib/View/home/homePage.dart
+++ lib/View/home/homePage.dart
@@ -10,6 +10,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final List routes = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
