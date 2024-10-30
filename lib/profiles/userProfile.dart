f159806e5909b23b8a024bb5e4cc77f483617df0
benilde Tobokoue <147486389+MrRAnonybot@users.noreply.github.com>
2024-10-30 06:18:26 +0100

    page profile user modification

--- lib/profiles/userProfile.dart
+++ lib/profiles/userProfile.dart
@@ -22,56 +22,57 @@ class _UserprofileState extends State<Userprofile> {
         ),
         backgroundColor: Colors.black,
       ),
-      body: Column(
-        children: [
-          const Expanded(flex: 3, child: _TopPortion()),
-          Expanded(
-            flex: 2,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  const Text(
-                    "Schedules",
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(height: 16),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      FloatingActionButton.extended(
-                        onPressed: () {},
-                        heroTag: 'event',
-                        elevation: 0,
-                        backgroundColor: Colors.yellow,
-                        label: const Text("Event"),
-                      ),
-                      const SizedBox(width: 16.0),
-                      FloatingActionButton.extended(
-                        onPressed: () {},
-                        heroTag: 'party',
-                        elevation: 0,
-                        backgroundColor: Colors.yellow,
-                        label: const Text("Party"),
-                      ),
-                      const SizedBox(width: 16.0),
-                      FloatingActionButton.extended(
-                        onPressed: () {},
-                        heroTag: 'lesson',
-                        elevation: 0,
-                        backgroundColor: Colors.yellow,
-                        label: const Text("Lesson"),
-                      ),
-                    ],
-                  ),
-                ],
+      body: SingleChildScrollView(
+        // Rendre le corps défilable
+        child: Column(
+          children: [
+            const _TopPortion(), // Suppression de Expanded
+            const SizedBox(
+                height: 16), // Espace entre la partie bleue et la carte
+            const _HorseCard(), // Ajout de la carte avec l'image
+            const SizedBox(height: 16), // Espace avant la section des horaires
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                "Schedules",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-          ),
-        ],
+            const SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FloatingActionButton.extended(
+                  onPressed: () {},
+                  heroTag: 'event',
+                  elevation: 0,
+                  backgroundColor: Colors.yellow,
+                  label: const Text("Event"),
+                ),
+                const SizedBox(width: 16.0),
+                FloatingActionButton.extended(
+                  onPressed: () {},
+                  heroTag: 'party',
+                  elevation: 0,
+                  backgroundColor: Colors.yellow,
+                  label: const Text("Party"),
+                ),
+                const SizedBox(width: 16.0),
+                FloatingActionButton.extended(
+                  onPressed: () {},
+                  heroTag: 'lesson',
+                  elevation: 0,
+                  backgroundColor: Colors.yellow,
+                  label: const Text("Lesson"),
+                ),
+              ],
+            ),
+            const SizedBox(height: 16), // Espace en bas
+          ],
+        ),
       ),
     );
   }
@@ -98,7 +99,7 @@ class _TopPortion extends StatelessWidget {
             ),
           ),
         ),
-        Positioned(
+        const Positioned(
           top: 40,
           left: 20,
           right: 20,
@@ -107,38 +108,38 @@ class _TopPortion extends StatelessWidget {
             children: [
               Row(
                 children: [
-                  const CircleAvatar(
+                  CircleAvatar(
                     radius: 50,
                     backgroundImage: NetworkImage(
                       "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
                     ),
                   ),
-                  const SizedBox(width: 16),
+                  SizedBox(width: 16),
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      const Text(
-                        "Dr. Stefeni Albert",
+                      Text(
+                        "Name",
                         style: TextStyle(
                           fontSize: 28,
                           color: Colors.white,
                           fontWeight: FontWeight.bold,
                         ),
                       ),
-                      const SizedBox(height: 8),
-                      const Text(
-                        "Heart Specialist",
+                      SizedBox(height: 8),
+                      Text(
+                        "Rider",
                         style: TextStyle(
                           fontSize: 18,
                           color: Colors.white70,
                         ),
                       ),
-                      const SizedBox(height: 12),
+                      SizedBox(height: 12),
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Row(
-                            children: const [
+                            children: [
                               Icon(Icons.cake, color: Colors.white70, size: 20),
                               SizedBox(width: 8),
                               Text(
@@ -150,9 +151,9 @@ class _TopPortion extends StatelessWidget {
                               ),
                             ],
                           ),
-                          const SizedBox(height: 8),
+                          SizedBox(height: 8),
                           Row(
-                            children: const [
+                            children: [
                               Icon(Icons.phone,
                                   color: Colors.white70, size: 20),
                               SizedBox(width: 8),
@@ -165,9 +166,9 @@ class _TopPortion extends StatelessWidget {
                               ),
                             ],
                           ),
-                          const SizedBox(height: 8),
+                          SizedBox(height: 8),
                           Row(
-                            children: const [
+                            children: [
                               Icon(Icons.link, color: Colors.white70, size: 20),
                               SizedBox(width: 8),
                               Text(
@@ -190,7 +191,7 @@ class _TopPortion extends StatelessWidget {
           ),
         ),
         Positioned(
-          bottom: 190, // Positionnement au bas du conteneur bleu
+          bottom: 10, // Positionnement au bas du conteneur bleu
           left: MediaQuery.of(context).size.width * 0.35,
           right: MediaQuery.of(context).size.width * 0.35,
           child: Container(
@@ -216,6 +217,72 @@ class _TopPortion extends StatelessWidget {
           ),
         ),
       ],
+    );
+  }
+}
+
+// Classe pour la carte des chevaux
+class _HorseCard extends StatelessWidget {
+  const _HorseCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Stack(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(15.0),
+            child: Image.network(
+              "https://pxhere.com/fr/photo/981167",
+              height: 200,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            height: 200,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.black.withOpacity(0.10), // Ombre sombre
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+          ),
+          Positioned(
+            top: 80,
+            left: 20,
+            right: 20,
+            child: Column(
+              children: [
+                const Text(
+                  "Mon/mes cheval/aux",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 8),
+                ElevatedButton(
+                  onPressed: () {
+                    // Action pour accéder à la page des chevaux
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.yellow, // Couleur du bouton
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: const Text(
+                    "Voir mes chevaux",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
