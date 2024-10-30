acd9d18d2f165b3a0215d5427e670987cadb70fe
Aurelien T. <60261287+Traylen@users.noreply.github.com>
2024-10-30 16:35:53 +0100

    fin backForm

---
+++ lib/model/concour.dart
@@ -0,0 +1,79 @@
+import 'package:chevaldetroie/model/database.dart';
+import 'package:mongo_dart/mongo_dart.dart';
+
+class Competition {
+  final String collection = "Competition";
+  Map<String, dynamic> data = <String, dynamic>{
+    'name': '',
+    'adresse': '',
+    'pp': '',
+    'date': '',
+  };
+
+  Competition setName(String name) {
+    data.update('name', (value) => name);
+    return this;
+  }
+
+  Competition setAdresse(String adresse) {
+    data.update('adresse', (value) => adresse);
+    return this;
+  }
+
+  Competition setPP(String pp) {
+    data.update('pp', (value) => pp);
+    return this;
+  }
+
+  Competition setDate(String date) {
+    data.update('date', (value) => date);
+    return this;
+  }
+
+  String getId() {
+    return data['_id'];
+  }
+
+  String getName() {
+    return data['name'];
+  }
+
+  String getAdresse() {
+    return data['adresse'];
+  }
+
+  String getPP() {
+    return data['pp'];
+  }
+
+  String getDate() {
+    return data['date'];
+  }
+
+  Future<Competition> findById(id) async {
+    var currentObjectId = id is String ? ObjectId.fromHexString(id) : id;
+    List<Map<String, dynamic>> list =
+        await Database().findByField(collection, '_id', currentObjectId);
+    data.addAll(list[0]);
+    return this;
+  }
+
+  Future<Competition> findFirstByField(String field, dynamic value) async {
+    List<Map<String, dynamic>> list =
+        await Database().findByField(collection, field, value);
+    data.addAll(list[0]);
+    return this;
+  }
+
+  Future<List<Map<String, dynamic>>> findAllByField(
+      String field, dynamic value) async {
+    List<Map<String, dynamic>> list =
+        await Database().findByField(collection, field, value);
+    return list;
+  }
+
+  void insert() {
+    if (data.isEmpty) throw ('Missing data');
+    Database().add(collection, data);
+  }
+}
