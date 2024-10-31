6141f9106df9bf83710fabfa327c0e76bb10ca8f
Aurelien T. <60261287+Traylen@users.noreply.github.com>
2024-10-31 09:41:56 +0100

    validationPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:developer' as developer;
 
 import 'package:chevaldetroie/model/rave.dart';
+import 'package:chevaldetroie/view/admin.dart';
 import 'package:chevaldetroie/view/concour.dart';
 import 'package:chevaldetroie/view/horseLesson.dart';
 import 'package:chevaldetroie/view/login.dart';
@@ -22,17 +23,17 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Cheval De Troie',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: HorseLesson(),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Cheval De Troie',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: RaveValidationPage(
+          raveId: '6723417e64dd2aa794000000',
+        ));
   }
 }
--- lib/model/database.dart
+++ lib/model/database.dart
@@ -1,24 +1,35 @@
 import 'package:mongo_dart/mongo_dart.dart';
 
 class Database {
+  final Db db = Db('mongodb://10.0.2.2:27017/flutter');
 
-Db db = Db('mongodb://10.0.2.2:27017/flutter');
-
-void add(String collection, Map<String, dynamic> data) async {
-  await db.open();
+  Future<void> add(String collection, Map<String, dynamic> data) async {
+    await db.open();
     await db.collection(collection).insert(data);
-  await db.close();
-}
+    await db.close();
+  }
 
-Future<List<Map<String, dynamic>>> findByField(collection, field, value, {rating = 10}) async {
-  await db.open();
-  var output = await db.collection(collection).find(where.eq(field, value)).toList();
-  await db.close();
-  return output;
-}
+  Future<List<Map<String, dynamic>>> findByField(
+      String collection, String field, dynamic value,
+      {int rating = 10}) async {
+    await db.open();
+    var output =
+        await db.collection(collection).find(where.eq(field, value)).toList();
+    await db.close();
+    return output;
+  }
 
-DbCollection collection(String collection){
-  return db.collection(collection);
-}
+  Future<void> update(
+      String collection, ObjectId id, Map<String, dynamic> data) async {
+    await db.open();
+    await db.collection(collection).updateOne(
+          where.id(id),
+          modify.set('validate', data['validate']),
+        );
+    await db.close();
+  }
 
+  DbCollection collection(String collectionName) {
+    return db.collection(collectionName);
+  }
 }
